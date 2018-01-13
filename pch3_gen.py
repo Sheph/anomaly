46eabc83e8d34e8f5e3fc4670630d7e19e0ea691
Stanislav Vorobiov <sheffmail@mail.ru>
2018-01-13 13:11:59 +0300

    some tracking fixes

--- pch3_gen.py
+++ pch3_gen.py
@@ -57,6 +57,42 @@ def process(cap, preds):
 
 		frm = cap.get(cv2.CAP_PROP_POS_FRAMES)
 
+		for tr in trackers:
+			tr_obj = tr[0]
+			bbox = tr[1]
+			ok2, ubbox = tr_obj.update(orig_frame)
+			ubbox = (int(ubbox[0]), int(ubbox[1]), int(ubbox[2]), int(ubbox[3]))
+			if ok2:
+				tr[2] = ubbox
+			else:
+				pass
+				#tr[2] = bbox
+
+		for tr in trackers:
+			tr_obj = tr[0]
+			bbox = tr[1]
+			ubbox = tr[2]
+			if ubbox is None:
+				continue
+			cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[0] + bbox[2], bbox[1] + bbox[3]), (0, 255, 0), 2)
+			cv2.rectangle(frame, (ubbox[0], ubbox[1]), (ubbox[0] + ubbox[2], ubbox[1] + ubbox[3]), (0, 0, 255), 2)
+
+			cx = bbox[0] + bbox[2] / 2.0
+			cy = bbox[1] + bbox[3] / 2.0
+			w = float(bbox[2])
+			h = float(bbox[3])
+			Rf = 0.0
+
+			mx = ubbox[0] + ubbox[2] / 2.0
+			my = ubbox[1] + ubbox[3] / 2.0
+
+			features.append([cap.get(cv2.CAP_PROP_POS_FRAMES) / fps, cap.get(cv2.CAP_PROP_POS_FRAMES), cx, cy, w, h, Rf, (mx - cx), (my - cy)])
+
+			tr[1] = tr[2]
+
+		if trackers:
+			cv2.imshow('frame', frame)
+
 		if frm >= cur_pred[0]:
 			boxes = cur_pred[1]
 			trackers = []
@@ -69,57 +105,22 @@ def process(cap, preds):
 				w = int(b[5] * frame_w)
 				h = int(b[6] * frame_h)
 
-				#if cls != "person":
-				#	continue
+				if cls != "person":
+					continue
 
 				if (w >= obj_min_w) & (h >= obj_min_h) & (w <= obj_max_w) & (h <= obj_max_h):
 					cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), 2)
+					# should use 0.1 as detect_thresh, but no python api :(
+					# change in c++ source
 					tr = cv2.TrackerKCF_create()
 					#tr = cv2.Tracker_create('KCF')
 					tr.init(orig_frame, (x, y, w, h))
 					tr.update(orig_frame)
 					trackers.append([tr, (x, y, w, h), (x, y, w, h)])
 
-			#cv2.imshow('frame', frame)
 			pred_i += 1
 			if pred_i >= len(preds):
 				break
-		else:
-			for tr in trackers:
-				tr_obj = tr[0]
-				bbox = tr[1]
-				#cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[0] + bbox[2], bbox[1] + bbox[3]), (0, 255, 0), 2)
-				ok2, ubbox = tr_obj.update(orig_frame)
-				ubbox = (int(ubbox[0]), int(ubbox[1]), int(ubbox[2]), int(ubbox[3]))
-				if ok2:
-					tr[2] = ubbox
-				else:
-					tr[2] = None
-					#cv2.rectangle(frame, (ubbox[0], ubbox[1]), (ubbox[0] + ubbox[2], ubbox[1] + ubbox[3]), (0, 0, 255), 2)
-			#cv2.imshow('frame', frame)
-
-			for tr in trackers:
-				tr_obj = tr[0]
-				bbox = tr[1]
-				ubbox = tr[2]
-				if ubbox is None:
-					continue
-				cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[0] + bbox[2], bbox[1] + bbox[3]), (0, 255, 0), 2)
-				cv2.rectangle(frame, (ubbox[0], ubbox[1]), (ubbox[0] + ubbox[2], ubbox[1] + ubbox[3]), (0, 0, 255), 2)
-
-				cx = bbox[0] + bbox[2] / 2.0
-				cy = bbox[1] + bbox[3] / 2.0
-				w = float(bbox[2])
-				h = float(bbox[3])
-				Rf = 0.0
-
-				mx = ubbox[0] + ubbox[2] / 2.0
-				my = ubbox[1] + ubbox[3] / 2.0
-
-				features.append([cap.get(cv2.CAP_PROP_POS_FRAMES) / fps, cap.get(cv2.CAP_PROP_POS_FRAMES), cx, cy, w, h, Rf, (mx - cx), (my - cy)])
-
-				tr[1] = tr[2]
-			cv2.imshow('frame', frame)
 
 		k = cv2.waitKey(1) & 0xff
 		if k == 32:
@@ -134,11 +135,15 @@ def process(cap, preds):
 if __name__ == "__main__":
 	#cap = cv2.VideoCapture('Datasets/UCSDPed1/combined/test.avi')
 	#process(cap, json.load(open('Datasets/UCSDPed1/combined/test_boxes.json')))
-	cap = cv2.VideoCapture('Datasets/Pedestrian/train.avi')
-	process(cap, json.load(open('ped_train_boxes.json')))
+	#cap = cv2.VideoCapture('Datasets/Pedestrian/train.avi')
+	#process(cap, json.load(open('ped_train_boxes.json')))
 	#cap = cv2.VideoCapture('Datasets/Crossroads1/train.avi')
 	#process(cap, json.load(open('cross1_train_boxes.json')))
 	#cap = cv2.VideoCapture('z3.avi')
 	#process(cap, json.load(open('z3_boxes.json')))
+
+	cap = cv2.VideoCapture('reception_long_test.avi')
+	process(cap, json.load(open('reception_long_test_boxes.json')))
+
 	cap.release()
 	cv2.destroyAllWindows()
